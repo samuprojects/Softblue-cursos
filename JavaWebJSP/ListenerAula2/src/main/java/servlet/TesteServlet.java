afadc9ba450ece54d375c2ad1d56b423ccbfda03
Samuel <samugit.projects@gmail.com>
2024-07-05 12:29:29 -0300

    Módulo10 Aula03 Usando a interface HttpSessionBindingListener

---
+++ JavaWebJSP/ListenerAula2/src/main/java/servlet/TesteServlet.java
@@ -0,0 +1,26 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Produto;
+
+@SuppressWarnings("serial")
+public class TesteServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		
+		Produto p = new Produto();
+		session.setAttribute("p1", p);
+		
+		String nomeProduto = p.getNome();
+		System.out.println(nomeProduto);
+	}
+}
