252b2e2e35988cfa05114ef64d958f419b8f57ab
Samuel <samugit.projects@gmail.com>
2024-07-12 22:19:14 -0300

    Módulo12 Aula05 Sistema de ensalamento camada Controller

--- JavaWebJSP/Ensalamento/src/main/java/action/AdicionarAlunoSalaAction.java
+++ JavaWebJSP/Ensalamento/src/main/java/action/AdicionarAlunoSalaAction.java
@@ -1,5 +1,18 @@
 package action;
 
-public class AdicionarAlunoSalaAction {
+import service.EnsalamentoService;
 
+public class AdicionarAlunoSalaAction extends Action {
+
+	@Override
+	public void process() throws Exception {
+		
+		String matricula = getRequest().getParameter("matricula");
+		int numSala = Integer.parseInt(getRequest().getParameter("numSala"));
+		
+		EnsalamentoService ensalamentoService = serviceFactory.getEnsalamentoService();
+		ensalamentoService.adicionarAlunoSala(matricula, numSala);
+		
+		redirect("ListarAlunos.action?numSala=" + numSala);
+	}
 }
--- JavaWebJSP/Ensalamento/src/main/java/action/ListarSalasAction.java
+++ JavaWebJSP/Ensalamento/src/main/java/action/ListarSalasAction.java
@@ -1,5 +1,19 @@
 package action;
 
-public class ListarSalasAction {
+import java.util.List;
 
+import entity.Sala;
+import service.SalaService;
+
+public class ListarSalasAction extends Action {
+
+	@Override
+	public void process() throws Exception {
+		
+		SalaService salaService = serviceFactory.getSalaService();
+		List<Sala> salas = salaService.getSalas();
+		
+		getRequest().setAttribute("salas", salas);
+		forward("lista_salas.jsp");
+	}
 }
