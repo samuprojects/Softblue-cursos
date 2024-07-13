cbbfc5fda796cdc6edfb4d863bafefa154e08dd7
Samuel <samugit.projects@gmail.com>
2024-07-12 21:11:37 -0300

    Módulo12 Aula03 Sistema de ensalamento camada de acesso a dados DAO

--- JavaWebJSP/Ensalamento/src/main/java/dao/DAOException.java
+++ JavaWebJSP/Ensalamento/src/main/java/dao/DAOException.java
@@ -1,5 +1,20 @@
 package dao;
 
-public class DAOException {
+@SuppressWarnings("serial")
+public class DAOException extends Exception {
 
+	public DAOException() {
+	}
+
+	public DAOException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DAOException(String message) {
+		super(message);
+	}
+
+	public DAOException(Throwable cause) {
+		super(cause);
+	}
 }
